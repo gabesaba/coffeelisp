1c1fe4ab637f5d37bb0183b869866a331ea020d5
Gabe <gabesaba@gmail.com>
2020-04-19 20:25:18 -0700

    Fix index of String. Move String to its own file

---
+++ src/main/kotlin/coffeelisp/types/LispString.kt
@@ -0,0 +1,19 @@
+package coffeelisp.types
+
+import coffeelisp.syntax.Atom
+
+data class LispString(val s: String): LispObject {
+    override fun display() = s
+
+    override fun type() = LispType("String")
+
+    companion object {
+        fun isType(atom: Atom): Boolean {
+            return atom.token.startsWith('"') && atom.token.endsWith('"')
+        }
+
+        fun toLispObject(atom: Atom): LispObject {
+            return LispString(atom.token.substring(1, atom.token.lastIndex))
+        }
+    }
+}
