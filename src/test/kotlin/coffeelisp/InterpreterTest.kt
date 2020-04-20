e107dd7ee694a1ddc9b34f2252515813d27b31dc
Gabe <15304068+gabesaba@users.noreply.github.com>
2020-04-20 10:12:59 -0700

    Reimplement Recursion limit
    When fixing lexical scoping, I created
    a regression where we no longer prevented
    infinite recursion.

--- src/test/kotlin/coffeelisp/InterpreterTest.kt
+++ src/test/kotlin/coffeelisp/InterpreterTest.kt
@@ -1,7 +1,9 @@
 package coffeelisp
 
 import coffeelisp.env.eval
+import coffeelisp.errors.RecursionDepthExceeded
 import kotlin.test.Test
+import kotlin.test.assertFailsWith
 
 class InterpreterTest {
     @Test
@@ -23,5 +25,13 @@ class InterpreterTest {
         "(define sub1 (lambda (x) (- x 1)))".eval()
         "(define fact (lambda (x) (if (zero? x) 1 (* x (fact (sub1 x))))))".eval()
         "(fact 6)".assertEquals("720")
+    }
+
+    @Test
+    fun testNoStackSmashing() {
+        "(define recur (lambda () (recur)))".eval()
+        assertFailsWith(RecursionDepthExceeded::class) {
+            "(recur)".eval()
+        }
     }
 }
